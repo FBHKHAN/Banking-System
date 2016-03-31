417c244265a5a5027ee0748144837868c0749f28
farooqkhan <fbhkhan92@gmail.com>
2016-03-31 19:26:48 -0400

    Initial Commit

---
+++ Banking System/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Banking System
+//
+//  Created by Farooq Khan on 3/31/16.
+//  Copyright © 2016 Farooq Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
